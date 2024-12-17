62b1e722db53d37216fa6d44ca9334bfebdae266
jgreiffo <jgreiffo@gmail.com>
2024-12-16 20:26:51 -0500

    Add files via upload
    ATM

---
+++ app.js
@@ -0,0 +1,12 @@
+function login(form){
+    if (form.u.value =="pedro") {
+        if (form.c.value=="123") {
+            location="principal.html";
+        }
+    } else {
+        alert("error de password");
+    }
+    }   else {
+        alert{"error de usuario"};
+    
+}
